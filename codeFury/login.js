42c02e8eb2ce973765e86280fb090a2e507fc7d5
Pawan Kumar <75720045+Pawan0kumar@users.noreply.github.com>
2023-09-05 22:24:37 +0530

    New Commit

---
+++ codeFury/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // You can replace the hardcoded username and password with your authentication logic
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'pawan' && password === 'pardon') {
+        // Redirect to the home page on successful login
+        window.location.href = 'home.html';
+    } else {
+        // Display an error message or handle unsuccessful login
+        alert('Invalid username or password. Please try again.');
+    }
+});
